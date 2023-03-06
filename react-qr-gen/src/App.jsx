35395e582f4255a5644949aec5a1425e31373567
Taylor Jensen <Taylorlcj@gmail.com>
2023-03-06 17:32:12 -0500

    1245

--- react-qr-gen/src/App.jsx
+++ react-qr-gen/src/App.jsx
@@ -1,9 +1,29 @@
+import QRCode from "qrcode";
+import { useState } from "react";
+
 function App() {
+	const [url, seturl] = useState("");
+	const [qrcode, setQrcode] = useState("");
+
+	const GenerateQRCode = () => {
+		QRCode.toDataURL(url, (err, url) => {
+			if (err) return console.error(err);
+
+			console.log(url);
+			setQrcode(url);
+		});
+	};
 	return (
 		<div className="app">
 			<h1>QR Code Generator</h1>
-			<input type="text" placeholder="e.g. https:google.com" />
-			<button>Generate</button>
+			<input
+				type="text"
+				placeholder="e.g. https:google.com"
+				value={url}
+				onChange={(evt) => seturl(evt.target.value)}
+			/>
+			<button onClick={GenerateQRCode}>Generate</button>
+			<img src="{qrcode}" />
 		</div>
 	);
 }
